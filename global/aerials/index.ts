6c0440261b34f6ee79ddfc175f522787115c9b3c
zarmot <dev@zarmot.com>
2023-07-29 17:24:09 +0800

    tool example

--- global/aerials/index.ts
+++ global/aerials/index.ts
@@ -1,9 +1,12 @@
 import * as aerials from "./aerials.js"
 import * as aerialsx from "./aerialsx.js"
+import * as tools from "./tools.js"
 
 declare global {
   var Aerials: typeof aerials
   var Aerialsx: typeof aerialsx
+  var Tools: typeof tools
 }
 global.Aerials = aerials
 global.Aerialsx = aerialsx
+global.Tools = tools
